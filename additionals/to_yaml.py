846521b0fa7ab355f984a04d1a73f1fc2f6e0c0a
srz_zumix <zumix.cpp@gmail.com>
2023-10-06 13:00:37 +0900

    add to_yaml (#12)
    * add to_yaml
    
    * fix
    
    * fix

---
+++ additionals/to_yaml.py
@@ -0,0 +1,39 @@
+from kamidana import (
+    as_filter,
+)
+from collections import OrderedDict
+from jinja2.filters import contextfilter
+import ruamel.yaml
+
+
+def represent_odict(dumper, instance):
+    return dumper.represent_mapping('tag:yaml.org,2002:map', instance.items())
+
+ruamel.yaml.add_representer(OrderedDict, represent_odict)
+
+
+def construct_odict(loader, node):
+    return OrderedDict(loader.construct_pairs(node))
+
+ruamel.yaml.add_constructor('tag:yaml.org,2002:map', construct_odict)
+
+
+@as_filter
+def to_yaml(ctx, a, *args, **kw):
+    yaml = ruamel.yaml.YAML(typ=['rt', 'string'])
+    yaml.default_flow_style = kw.pop('default_flow_style', None)
+    yaml.allow_unicode=True
+    yaml.indent=kw.pop('indent', 2)
+    transformed = yaml.dump_to_string(a)
+    return transformed
+
+
+@as_filter
+@contextfilter
+def to_nice_yaml(ctx, a, indent=2, *args, **kw):
+    yaml = ruamel.yaml.YAML(typ=['rt', 'string'])
+    yaml.default_flow_style=False
+    yaml.allow_unicode=True
+    yaml.indent=indent
+    transformed = yaml.dump_to_string(a)
+    return transformed
