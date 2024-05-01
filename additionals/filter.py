3fb26cdb6433086d5b6405ac1a5a3484e02e5446
srz_zumix <zumix.cpp@gmail.com>
2024-05-01 14:01:11 +0900

    fix

--- additionals/filter.py
+++ additionals/filter.py
@@ -10,38 +10,38 @@ import re
 @as_filter
 @pass_context
 def regex_replace(ctx, v, pattern, repl, count=0, flags=0):
-    return re.sub(pattern, repl, v, count, flags)
+  return re.sub(pattern, repl, v, count, flags)
 
 
 @as_filter
 @pass_context
 def ternary(ctx, v, true_val, false_val, null_val=None):
-    if v:
-        return true_val
-    if null_val is not None and v is None:
-        return null_val
-    return false_val
+  if v:
+    return true_val
+  if null_val is not None and v is None:
+    return null_val
+  return false_val
 
 
 @as_filter
 @pass_context
 def b64encode(ctx, v, *, encoding='utf-8'):
-    return base64.b64encode(v.encode(encoding))
+  return base64.b64encode(v.encode(encoding))
 
 
 @as_filter
 @pass_context
 def b64decode(ctx, v, *, encoding='utf-8'):
-    return base64.b64decode(v).decode(encoding)
+  return base64.b64decode(v).decode(encoding)
 
 
 @as_filter
 @pass_context
 def json_query(ctx, v, query):
-    return jmespath.search(query, v)
+  return jmespath.search(query, v)
 
 
 @as_filter
 @pass_context
 def typeof(ctx, v):
-    return type(v)
+  return type(v)
--- additionals/to_yaml.py
+++ additionals/to_yaml.py
@@ -7,13 +7,13 @@ import ruamel.yaml
 
 
 def represent_odict(dumper, instance):
-    return dumper.represent_mapping('tag:yaml.org,2002:map', instance.items())
+  return dumper.represent_mapping('tag:yaml.org,2002:map', instance.items())
 
 ruamel.yaml.add_representer(OrderedDict, represent_odict)
 
 
 def construct_odict(loader, node):
-    return OrderedDict(loader.construct_pairs(node))
+  return OrderedDict(loader.construct_pairs(node))
 
 ruamel.yaml.add_constructor('tag:yaml.org,2002:map', construct_odict)
 
@@ -21,20 +21,20 @@ ruamel.yaml.add_constructor('tag:yaml.org,2002:map', construct_odict)
 @as_filter
 @pass_context
 def to_yaml(ctx, a, *args, **kw):
-    yaml = ruamel.yaml.YAML(typ=['rt', 'string'])
-    yaml.default_flow_style = kw.pop('default_flow_style', None)
-    yaml.allow_unicode=True
-    yaml.indent=kw.pop('indent', 2)
-    transformed = yaml.dump_to_string(a)
-    return transformed
+  yaml = ruamel.yaml.YAML(typ=['rt', 'string'])
+  yaml.default_flow_style = kw.pop('default_flow_style', None)
+  yaml.allow_unicode=True
+  yaml.indent=kw.pop('indent', 2)
+  transformed = yaml.dump_to_string(a)
+  return transformed
 
 
 @as_filter
 @pass_context
 def to_nice_yaml(ctx, a, indent=2, *args, **kw):
-    yaml = ruamel.yaml.YAML(typ=['rt', 'string'])
-    yaml.default_flow_style=False
-    yaml.allow_unicode=True
-    yaml.indent=indent
-    transformed = yaml.dump_to_string(a)
-    return transformed
+  yaml = ruamel.yaml.YAML(typ=['rt', 'string'])
+  yaml.default_flow_style=False
+  yaml.allow_unicode=True
+  yaml.indent=indent
+  transformed = yaml.dump_to_string(a)
+  return transformed
