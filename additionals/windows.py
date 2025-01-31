96dd9e8a2d7171ad9449c683d9244f0dd1fc6cf4
srz_zumix <zumix.cpp@gmail.com>
2025-01-31 13:28:47 +0900

    test

--- additionals/windows.py
+++ additionals/windows.py
@@ -3,5 +3,5 @@ import sys
 
 if sys.platform == "win32":
   import os, msvcrt
-  msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY) if os.name == 'nt':
+  msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
   os.linesep = '\n'
